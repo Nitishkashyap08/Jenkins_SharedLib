5ec302254c70e3b20fa5d9e6f93e7c9e0ecc95aa
nitish kashyap <kashyapnitishkn@gmail.com>
2025-05-20 22:51:16 +0530

    jjk

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(){
+  sh "docker build -t nitishkashyap08/mega-project:v2 ."
 }
