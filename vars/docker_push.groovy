132706e9da66becc4c314a84e39ce58c328aba36
nitish kashyap <kashyapnitishkn@gmail.com>
2025-05-20 21:37:50 +0530

    ffs

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,7 @@
 def call(String Project, String ImageTag, String dockerhubuser) {
     withCredentials([string(credentialsId: 'docker-nitishkashyap08', variable: 'dockerhubpass')]) {
-        sh 'echo "$dockerhubpass" | docker login -u nitishkashyap08 --password-stdin'
+        sh "echo \"$dockerhubpass\" | docker login -u nitishkashyap08 --password-stdin"
     }
     sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
+
